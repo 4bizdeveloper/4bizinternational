71c1a5d9c08761354b6b082fdbb17f719bbf2793
4bizdeveloper <4bizdeveloper@gmail.com>
2026-06-16 22:23:50 +0530

    today

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -157,7 +157,7 @@ export default function Hero() {
                 aspectRatio: '16/9'
               }}
             >
-              <source src="/hero-background-video-1.mp4" type="video/mp4" />
+              <source src="/hero-background-video-2.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
 
