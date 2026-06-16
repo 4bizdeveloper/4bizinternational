42800d30412d62e8c81c90d7420bad0c6b538128
4bizdeveloper <4bizdeveloper@gmail.com>
2026-06-16 11:55:39 +0530

    today

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -81,7 +81,7 @@ const Footer = () => {
       ref={footerRef} 
       className="relative z-10 mt-0 pt-16 pb-32 md:pb-24 overflow-hidden border-t border-white/10 transform-gpu backface-hidden"
       style={{ 
-        backgroundColor: '#00192d',
+        backgroundColor: '#0b1536',
         contentVisibility: 'auto', 
         paddingBottom: 'calc(120px + env(safe-area-inset-bottom))'
       } as React.CSSProperties}
